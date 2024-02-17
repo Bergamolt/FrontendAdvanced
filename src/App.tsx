f8115163cd3884bf235ab31fec5bc7802afb22f9
Valeriy Khoma <bergamolt22@gmail.com>
2024-02-18 00:37:54 +0200

    Implement theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
+import cx from 'classyfier'
 import { HomePageAsync } from './pages/Home'
 import { AboutPageAsync } from './pages/About'
 import './styles/index.scss'
+import { useTheme } from './theme/context'
 
 export function App() {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className='app light'>
+    <div className={cx('app', theme)}>
+      <button onClick={toggleTheme}>Change theme</button>
       <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
       <Suspense fallback={<span>Loading...</span>}>
