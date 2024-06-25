adcc60896b74d322588719078952ae8ffbe1eae8
Valeriy Khoma <bergamolt22@gmail.com>
2024-06-25 14:25:04 +0300

    Add UI tests for Button component and Navbar component

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -30,4 +30,44 @@ describe('Button', () => {
 
     expect(handleClick).toHaveBeenCalled()
   })
+
+  it('renders a button element with small size', () => {
+    render(<Button size='small'>Click me</Button>)
+
+    const button = screen.getByRole('button')
+
+    expect(button.classList).toContain('small')
+  })
+
+  it('renders a button element with medium size', () => {
+    render(<Button size='medium'>Click me</Button>)
+
+    const button = screen.getByRole('button')
+
+    expect(button.classList).toContain('medium')
+  })
+
+  it('renders a button element with large size', () => {
+    render(<Button size='large'>Click me</Button>)
+
+    const button = screen.getByRole('button')
+
+    expect(button.classList).toContain('large')
+  })
+
+  it('renders a button element with xlarge size', () => {
+    render(<Button size='xlarge'>Click me</Button>)
+
+    const button = screen.getByRole('button')
+
+    expect(button.classList).toContain('xlarge')
+  })
+
+  it('renders a button element with outlined mode', () => {
+    render(<Button mode='outlined'>Click me</Button>)
+
+    const button = screen.getByRole('button')
+
+    expect(button.classList).toContain('outlined')
+  })
 })
